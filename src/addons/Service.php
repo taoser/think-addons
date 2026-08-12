6055947fadea522bbd2c6dd5b133a640712cfbf8
无邪 <changlin_zhao@qq.com>
2026-08-12 13:59:32 +0800

    3.0

--- src/addons/Service.php
+++ src/addons/Service.php
@@ -6,11 +6,9 @@ namespace taoser\addons;
 use think\Route;
 use think\helper\Str;
 use think\facade\Config;
-use think\facade\Lang;
 use think\facade\Cache;
 use think\facade\Event;
 use taoser\addons\middleware\Addons;
-use think\facade\Request;
 
 /**
  * 插件服务
@@ -24,227 +22,109 @@ class Service extends \think\Service
     // 注册系统服务，将服务绑定到容器中
     public function register()
     {
-        $this->addons_path = $this->getAddonsPath();
-        // 加载系统语言包
-        $this->loadLang();
-        // 自动载入插件
-        $this->autoload();
-        // 加载插件事件
-        $this->loadEvent();
-        // // 加载自定义路由
-        $this->loadRoutes();
-        // // 加载插件系统服务
-        $this->loadService();
-        // // 加载插件命令
-        // $this->loadCommand();
-        // // 加载配置
-        // $this->loadConfig();
-        // 绑定插件容器
-        $this->app->bind('addons', Service::class);
+        $this->app->bind('addons', AddonsSystem::class);        
+
+        // $this->initLazyLoader();
         
     }
 
     public function boot()
     {
+
+        $addonsSystem = new AddonsSystem();
+
+        $addonsSystem->autoload();
+        // $addonsSystem->loadEvent();
+
         // 立即注册路由
         $route = $this->app->route;
         $execute = '\\taoser\\addons\\Route::execute';
-        
+
+        $middlewareArr = [Addons::class];
         // 注册插件公共中间件
         if (is_file($this->app->addons->getAddonsPath() . 'middleware.php')) {
-            $this->app->middleware->import(include $this->app->addons->getAddonsPath() . 'middleware.php', 'route');
+            $middlewareArr = array_merge($middlewareArr, include $this->app->addons->getAddonsPath() . 'middleware.php');
         }
+
         // 注册插件控制器路由
-        // $route->rule("addons/:addon/[:controller]/[:action]", $execute)->middleware(Addons::class);
-        $route->rule("app/:addon/[:controller]/[:action]", $execute);
+        $route->rule("app/:addon/[:controller]/[:action]", $execute)->middleware($middlewareArr);
         
-        // $route->group('addons', function () use ($route, $execute) {
-        //     $route->rule(':addon/:controller/:action', ':controller/:action');
-        // })
-        // // ->middleware(\app\middleware\AccessControl::class)
-        // ->namespace('addons\:addon\controller');
-
+        /**** 监听注册路由
         // 监听注册路由
-
-        // $this->registerRoutes(function (Route $route) {
+        $this->registerRoutes(function (Route $route) {
             
-        //     // 路由脚本
-        //     $execute = '\\taoser\\addons\\Route::execute';
+            // 路由脚本
+            $execute = '\\taoser\\addons\\Route::execute';
 
-        //     // 注册插件公共中间件
-        //     if (is_file($this->app->addons->getAddonsPath() . 'middleware.php')) {
-        //         // $this->app->middleware->import(include $this->app->addons->getAddonsPath() . 'middleware.php', 'route');
-        //     }
-
-        //     // 注册控制器路由
-        //     // $route->rule("addons/:addon/[:controller]/[:action]", $execute)->middleware(Addons::class);
-            
-        //     // 使用路由分组
-        //     $route->group('addons',function () use ($route, $execute){
-        //         $route->rule(':addon/:controller/:action',':controller/:action');
-        //         // $route->rule(':addon/:controller/:action', $execute);
-        //     })
-        //     ->middleware(\app\middleware\AccessControl::class)
-        //     ->namespace('addons\:addon\controller');
-
-        //     // 自定义路由
-        //     // $routes = (array) Config::get('addons.route', []);
-
-        //     // if(!empty($routes)) {
-        //     //     foreach ($routes as $key => $val) {
-        //     //         if (!$val) {
-        //     //             continue;
-        //     //         }
-        //     //         if (is_array($val)) {
-        //     //             $domain = $val['domain'];
-        //     //             $rules = [];
-        //     //             foreach ($val['rule'] as $k => $rule) {
-        //     //                 [$addon, $controller, $action] = explode('/', $rule);
-        //     //                 $rules[$k] = [
-        //     //                     'addon'        => $addon,
-        //     //                     'controller'    => $controller,
-        //     //                     'action'        => $action,
-        //     //                     'indomain'      => 1,
-        //     //                 ];
-        //     //             }
-        //     //             $route->domain($domain, function () use ($rules, $route, $execute) {
-        //     //                 // 动态注册域名的路由规则
-        //     //                 foreach ($rules as $k => $rule) {
-        //     //                     $route->rule($k, $execute)
-        //     //                         ->name($k)
-        //     //                         ->completeMatch(true)
-        //     //                         ->append($rule);
-        //     //                 }
-        //     //             });
-        //     //         } else {
-        //     //             list($addon, $controller, $action) = explode('/', $val);
-        //     //             $route->rule($key, $execute)
-        //     //                 ->name($key)
-        //     //                 ->completeMatch(true)
-        //     //                 ->append([
-        //     //                     'addon' => $addon,
-        //     //                     'controller' => $controller,
-        //     //                     'action' => $action
-        //     //                 ]);
-        //     //         }
-        //     //     }
-        //     // }
-
-        // });
-
-    }
-
-    private function loadLang()
-    {
-        Lang::load([
-            $this->app->getRootPath() . '/vendor/taoser/think-addons/src/lang/zh-cn.php'
-        ]);
-    }
-
-    /**
-     *  自定义路由文件
-     */
-    private function loadRoutes()
-    {
-        //配置
-        $addons_dir = Cache::get('addons_list');
-
-        // 如果缓存不存在，直接扫描插件目录
-        if (empty($addons_dir)) {
-            $addons_dir = scandir($this->addons_path);
-        }
-        
-        // 如果仍然为空，直接返回
-        if (empty($addons_dir)) {
-            return;
-        }
-  
-        foreach ($addons_dir as $name) {
-            // 跳过 . 和 ..
-            if (in_array($name, ['.', '..'])) {
-                continue;
+            $middlewareArr = [Addons::class];
+            // 注册插件公共中间件
+            if (is_file($this->app->addons->getAddonsPath() . 'middleware.php')) {
+                $middlewareArr = array_merge($middlewareArr, include $this->app->addons->getAddonsPath() . 'middleware.php');
             }
-            // 确保是目录
-            if (!is_dir($this->addons_path . $name)) {
-                continue;
-            }
-            
-            $module_dir = $this->addons_path . $name . DIRECTORY_SEPARATOR;
-            // 路由配置文件目录
-            $addons_route_dir = $module_dir . 'route' . DIRECTORY_SEPARATOR;
-            // 语言配置文件目录
-            $addons_lang_dir = $module_dir . 'lang' . DIRECTORY_SEPARATOR;
-            
-            if (file_exists($addons_route_dir) && is_dir($addons_route_dir)) {
-                $routeFiles = glob($addons_route_dir . '*.php');
-                foreach ($routeFiles as $route_file) {
-                    if (file_exists($route_file)) {
-                        // $this->loadRoutesFrom($file);
-                        include_once $route_file;
+
+            // 注册插件控制器路由
+            $route->rule("app/:addon/[:controller]/[:action]", $execute)->middleware($middlewareArr);
+
+            // 自定义路由
+            $routes = (array) Config::get('addons.route', []);
+
+            if(!empty($routes)) {
+                foreach ($routes as $key => $val) {
+                    if (!$val) {
+                        continue;
                     }
-                }
-            }
-            if (file_exists($addons_lang_dir) && is_dir($addons_lang_dir)) {
-                $langFiles = glob($addons_lang_dir . '*.php');
-                $langFilesArray = [];
-                foreach ($langFiles as $lang_file) {
-                    if (file_exists($lang_file)) {
-                        $langFilesArray[] = $lang_file;
-                    }
-                }
-                Lang::load($langFilesArray);
-            }
-        }
-    }
-
-     /**
-     * 加载插件配置文件
-     */
-    private function loadConfig()
-    {
-        $results = scandir($this->addons_path);
-        foreach ($results as $name) {
-            if (in_array($name, ['.', '..'])) continue;
-            if (!is_dir($this->addons_path . $name)) continue;
-            foreach (scandir($this->addons_path . $name) as $childname) {
-                if (in_array($childname, ['.', '..', 'public', 'view'])) {
-                    continue;
-                }
-             
-                $commands = [];
-                //配置文件
-                $addons_config_dir = $this->addons_path . $name . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR;
-
-                if (is_dir($addons_config_dir)) {
-
-                    $files = glob($addons_config_dir . '*.php');
-                    
-                    foreach ($files as $file) {
-                        if (file_exists($file)) {
-                            if (substr($file, -11) == 'console.php') {
-                                $commands_config = include_once $file;
-                                isset($commands_config['commands']) && $commands = array_merge($commands, $commands_config['commands']);
-                                !empty($commands) && $this->commands($commands);
-                            }
+                    if (is_array($val)) {
+                        $domain = $val['domain'];
+                        $rules = [];
+                        foreach ($val['rule'] as $k => $rule) {
+                            [$addon, $controller, $action] = explode('/', $rule);
+                            $rules[$k] = [
+                                'addon'        => $addon,
+                                'controller'    => $controller,
+                                'action'        => $action,
+                                'indomain'      => 1,
+                            ];
                         }
+                        $route->domain($domain, function () use ($rules, $route, $execute) {
+                            // 动态注册域名的路由规则
+                            foreach ($rules as $k => $rule) {
+                                $route->rule($k, $execute)
+                                    ->name($k)
+                                    ->completeMatch(true)
+                                    ->append($rule);
+                            }
+                        });
+                    } else {
+                        list($addon, $controller, $action) = explode('/', $val);
+                        $route->rule($key, $execute)
+                            ->name($key)
+                            ->completeMatch(true)
+                            ->append([
+                                'addon' => $addon,
+                                'controller' => $controller,
+                                'action' => $action
+                            ]);
                     }
                 }
-
             }
-        }
+
+        });
+
+        ****/
 
     }
 
     /**
-     * 插件事件
+     * 初始化延迟加载器
      */
-    private function loadEvent()
+    private function initLazyLoader(): void
     {
+        $loader = HookLazyLoader::getInstance();
+        
         $hooks = $this->app->isDebug() ? [] : Cache::get('hooks', []);
         
         if (empty($hooks)) {
             $hooks = (array) Config::get('addons.hooks', []);
-            // 初始化钩子
             foreach ($hooks as $key => $values) {
                 if (is_string($values)) {
                     $values = explode(',', $values);
@@ -257,161 +137,32 @@ class Service extends \think\Service
             }
             Cache::set('hooks', $hooks);
         }
-        //如果在插件中有定义 AddonsInit，则直接执行
-        if (isset($hooks['AddonsInit'])) {
-            foreach ($hooks['AddonsInit'] as $k => $v) {
-                Event::trigger('AddonsInit', $v);
+
+        foreach ($hooks as $hookName => $listeners) {
+            if (!empty($listeners)) {
+                $loader->registerHook($hookName, $listeners);
             }
         }
-        Event::listenEvents($hooks);
     }
+
 
     /**
-     * 挂载插件服务
+     * 注册延迟加载的钩子
+     * 只在钩子首次被触发时才加载对应的监听器
      */
-    private function loadService()
+    private function registerLazyHooks(array $hooks): void
     {
-        $results = scandir($this->addons_path);
-  
-        $bind = [];
-        foreach ($results as $name) {
-            if ($name === '.' or $name === '..') {
+        foreach ($hooks as $hookName => $listeners) {
+            if (empty($listeners)) {
                 continue;
             }
-            if (is_file($this->addons_path . $name)) {
-                continue;
-            }
-            $addonDir = $this->addons_path . $name . DIRECTORY_SEPARATOR;
-            if (!is_dir($addonDir)) {
-                continue;
-            }
-            if (!is_file($addonDir . 'Plugin.php')) {
-                continue;
-            }
-            $info_file = $addonDir . 'service.ini';
-            if (!is_file($info_file)) {
-                continue;
-            }
- 
-            $info = parse_ini_file($info_file, true, INI_SCANNER_TYPED) ?: [];
 
-            $bind = array_merge($bind, $info);
-        }
-
-        $this->app->bind($bind);
-    }
-
-    /**
-     * 自动载入插件
-     * @return bool
-     */
-    private function autoload()
-    {
-        // 是否处理自动载入
-        if (!Config::get('addons.autoload', true)) {
-            return true;
-        }
-
-        $conf = Cache::remember('addons_config', function(){
-            $config = $this->app->config->get('addons');
-            // 读取插件目录及钩子列表
-            $base = get_class_methods("\\taoser\\Addons");
-            $base = array_unique(array_merge($base, ['initialize','install', 'uninstall', 'enabled', 'disabled']));
-
-            $addon_list = [];
-
-            // 读取插件目录中的php文件
-            foreach (glob($this->getAddonsPath() . '*/*.php') as $addons_file) {
-                // 格式化路径信息
-                $info = pathinfo($addons_file);
-                // 获取插件目录名
-                $name = pathinfo($info['dirname'], PATHINFO_FILENAME);
-    
-                // 找到插件入口文件
-                if (strtolower($info['filename']) === 'plugin') {
-                    // 读取出所有公共方法
-                    $methods = (array)get_class_methods("\\addons\\" . $name . "\\" . $info['filename']);
-                    $ini= $info['dirname'] .DS. 'info.ini';
-                    if (!is_file($ini)) {
-                        continue;
-                    }
-                    $addon_ini = parse_ini_file($ini, true, INI_SCANNER_TYPED) ?: [];
-                    // dump(!$addon_ini['install']);
-                    // 排除未安装和未启用
-                    if(!$addon_ini['install']) continue;
-                    if(!$addon_ini['status']) continue;
-
-                    if(!in_array($name, $addon_list)) {
-                        $addon_list[] = $name;
-                    }
-
-                     //路由配置文件
-                    // $addons_route_dir = $module_dir . 'route' .  DIRECTORY_SEPARATOR;
-
-                    // if (file_exists($addons_route_dir) && is_dir($addons_route_dir)) {
-                    //     $files = glob($addons_route_dir . '*.php');
-                    //     foreach ($files as $file) {
-                    //         if (file_exists($file)) {
-                    //             $this->loadRoutesFrom($file);;
-                    //         }
-                    //     }
-                    // }
-                    
-                    // 跟插件基类方法做比对，得到差异结果
-                    $hooks = array_diff($methods, $base);
-                    // 循环将钩子方法写入配置中
-                    foreach ($hooks as $hook) {
-                        if (!isset($config['hooks'][$hook])) {
-                            $config['hooks'][$hook] = [];
-                        }
-                        // 兼容手动配置项
-                        if (is_string($config['hooks'][$hook])) {
-                            $config['hooks'][$hook] = explode(',', $config['hooks'][$hook]);
-                        }
-                        if (!in_array($name, $config['hooks'][$hook])) {
-                            $config['hooks'][$hook][] = $name;
-                        }
-                    }
-                }
-            }
-
-            Cache::set('addons_config', $config);
-            Cache::set('addons_list', $addon_list);
-
-            return $config;
-        });
-
-// dump($conf);
-
-        Config::set($conf, 'addons');
-        
-    }
-
-    /**
-     * 加载插件命令
-     */
-    private function loadCommand()
-    {
-        $results = scandir($this->addons_path);
-        foreach ($results as $name) {
-            if ($name === '.' or $name === '..') {
-                continue;
-            }
-            if (is_file($this->addons_path . $name)) {
-                continue;
-            }
-            $addonDir = $this->addons_path . $name . DIRECTORY_SEPARATOR;
-            if (!is_dir($addonDir)) {
-                continue;
-            }
-            $command_file = $addonDir . 'command.php';
-            if (is_file($command_file)) {
-                $commands = include_once $command_file;
-                if (is_array($commands))
-                    $this->commands($commands);
-            }
+            Event::listen($hookName, function ($params) use ($hookName, $listeners) {
+                return HookProxy::execute($hookName, $listeners, $params);
+            });
         }
     }
+
 
     /**
      * 获取 addons 路径
